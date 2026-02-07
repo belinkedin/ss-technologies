3a37624fe44bcb297184c8cf89d33633985ee747
belinkedin <belinkedindindigul@gmail.com>
2026-02-07 13:04:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/ss-technologies---all-in-one-management/',
+    base: '/ss-technologies/',
     server: {
       port: 3000,
       host: '0.0.0.0',
